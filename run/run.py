3bd7128e64447bf697a109701ca2e8cc3e1c3ef5
Ekadashi Pradhan <ekadashi@srv-k07-14.cbls.ccr.buffalo.edu>
2016-03-01 17:28:32 -0500

    simple QS input reading and writing

--- run/run.py
+++ run/run.py
@@ -2,17 +2,22 @@ import os
 import sys
 import math
 
+
+libra_bin_path = "/projects/academic/alexeyak/ekadashi/libracode-dev/libracode-code/_build/src"
+libra_espresso_int_path = "/projects/academic/alexeyak/ekadashi/devel/libra-qe_interface/src"
 # Path the the source code
 # sys.path.insert(1,"/projects/academic/alexeyak/kosukesa/dev/libra-gamess_interface/src")
-sys.path.insert(1,"/projects/academic/alexeyak/ekadashi/dev/libra-qe_interface/src")
+os.environ["src_path"] = libra_espresso_int_path
+sys.path.insert(1,os.environ["src_path"])    # path to the source code
+#sys.path.insert(1,"/projects/academic/alexeyak/ekadashi/dev/libra-qe_interface/src")
 
-cwd = "/projects/academic/alexeyak/alexeyak/libra-dev/libracode-code"
+cwd = "/projects/academic/alexeyak/ekadashi/libra-dev/libracode-code"
 print "Using the Libra installation at", cwd
-sys.path.insert(1,cwd+"/_build/src/mmath")
-sys.path.insert(1,cwd+"/_build/src/qchem")
-sys.path.insert(1,cwd+"_build/src/dyn")
-sys.path.insert(1,cwd+"_build/src/chemobjects")
-sys.path.insert(1,cwd+"_build/src/hamiltonian")
+#sys.path.insert(1,cwd+"/_build/src/mmath")
+#sys.path.insert(1,cwd+"/_build/src/qchem")
+#sys.path.insert(1,cwd+"_build/src/dyn")
+#sys.path.insert(1,cwd+"_build/src/chemobjects")
+#sys.path.insert(1,cwd+"_build/src/hamiltonian")
 
 
 ########## Setup all manual parameters here ####################
@@ -23,9 +28,9 @@ params["scr_dir"]=os.environ['SLURMTMPDIR']
 #params["gms_inp0"] = "H2O.inp"    # initial input file
 #params["gms_inp"] = "H2O_wrk.inp" # working input file 
 #params["gms_out"] = "H2O.out"  # output file
-params["qe_inp0"] = "H2O.inp"    # initial input file
-params["qe_inp"] = "H2O_wrk.inp" # working input file 
-params["qe_out"] = "H2O.out"  # output file
+params["qe_inp0"] = "x.md.in"    # initial input file
+params["qe_inp"] = "x.md_wrk.in" # working input file 
+params["qe_out"] = "x.md.out"  # output file
 params["nproc"] = 1             # the number of processors
 params["basis_option"]=2 # ab initio or Semi-Empirical calculation?  Options: \"ab_initio\" = 1 , \"semi_empirical\" = 2
 params["dt_nucl"]=20.0  # time step for nuclear dynamics  ex) 20 a.u. = 0.5 fsec
@@ -37,6 +42,10 @@ params["ene_file"] = params["res"]+"ene.xyz"
 
 
 ################################################################
+
+from path_libra_lib import * # import path_libra_lib module
+path_libra_lib(libra_bin_path) # Path to the libra libraries
+
 
 import main        # import main module of the libra-Gamess-interface code
 
--- src/main.py
+++ src/main.py
@@ -13,52 +13,58 @@
 # This module defines the function which communicates QUANTUM ESPRESSO output data
 # to Libra and vice versa.
 # It outputs the files needed for excited electron dynamics simulation.
-import sys
-
-# Path the the source code
-sys.path.insert(1,"/user/alexeyak/Programming/libra-qe_interface/src")
-
-cwd = "/projects/academic/alexeyak/alexeyak/libra-dev/libracode-code"
-print "Using the Libra installation at", cwd
-sys.path.insert(1,cwd+"/_build/src/mmath")
-sys.path.insert(1,cwd+"/_build/src/qchem")
-sys.path.insert(1,cwd+"/_build/src/dyn")
-sys.path.insert(1,cwd+"/_build/src/chemobjects")
-sys.path.insert(1,cwd+"/_build/src/hamiltonian")
-
-
 import os
 import sys
 import math
-# Needs to be modified ** ekadashi
-#from gamess_to_libra import *
-#from espresso_to_libra import *
-#from nve import *
-#from create_espresso_input import *
-#from create_gamess_input import *
-# ***ekadashi
 
-def read_qe_input_template():
-    pass
+# Path the the source code
+# First, we add the location of the library to test to the PYTHON path
+sys.path.insert(1,os.environ["src_path"]) # Path the the source code
+sys.path.insert(1,os.environ["libra_mmath_path"])
+sys.path.insert(1,os.environ["libra_qchem_path"])
+sys.path.insert(1,os.environ["libra_dyn_path"])
+sys.path.insert(1,os.environ["libra_chemobjects_path"])
+sys.path.insert(1,os.environ["libra_hamiltonian_path"])
+
+
+#from read_qe_inp_templ import*
+
+def read_qe_inp_templ(inp_filename):
+    f = open(inp_filename,"r")
+    templ = f.readlines()
+    f.close()
+
+    N = len(templ)
+    for i in range(0,N):
+        s = templ[i].split()
+        if len(s) > 0 and s[0] == "ATOMIC_POSITIONS":
+            ikeep = i 
+            break
+
+    templ[ikeep+1:N] = []
+    for i in xrange(ikeep+1):
+	print templ[i]
+
+    return templ
+    
+
 
 def main(params):
     ##
     # Finds the keywords and their patterns and extracts the parameters
     # \param[in] params : the input data from "submit_templ.slm", in the form of dictionary
-    # This function prepares initial parameters from GAMESS output file
-    # and executes classical MD in Libra and Electronic Structure Calculation in GAMESS 
+    # This function prepares initial parameters from QUANTUM ESPRESSO output file
+    # and executes classical MD in Libra and Electronic Structure Calculation in QUANTUM ESPRESSO
     # iteratively.
     #
     # Used in:  main.py
 
     ################# Step 0: Use the initial file to create a working input file ###############
  
-#    os.system("cp %s %s" %(params["gms_inp0"], params["gms_inp"]))
     os.system("cp %s %s" %(params["qe_inp0"], params["qe_inp"]))
 
-    ################# Step 1: Read initial input and run first GMS calculation ##################    
+    ################# Step 1: Read initial input and run first QS calculation ##################    
     
-#    params["gms_inp_templ"] = read_gms_inp_templ(params["gms_inp"])
     params["qe_inp_templ"] = read_qe_inp_templ(params["qe_inp"])
 
 #    exe_espresso(params)
